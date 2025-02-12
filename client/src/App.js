35875eff81db19cfe558e713464a005c205548df
Joonas Kallio <kallio.joonas@proton.me>
2025-02-12 16:22:30 +0200

    sidebar updated

--- client/src/App.js
+++ client/src/App.js
@@ -18,7 +18,7 @@ function App() {
   const [userData, setUserData] = useState(null);
   const [activeView, setActiveView] = useState("profile");
   const [authView, setAuthView] = useState("login");
-  const [dropdownOpen, setDropdownOpen] = useState(false);
+  const [sidebarOpen, setsidebarOpen] = useState(false);
   const [completedTrainings, setCompletedTrainings] = useState([]);
 
   useEffect(() => {
@@ -121,45 +121,30 @@ function App() {
     localStorage.removeItem("jwtToken");
     setUserData(null);
     setActiveView("login");
-    setDropdownOpen(false);
   };
 
   const handleSettings = () => {
     setActiveView("settings");
-    setDropdownOpen(false); // Close the dropdown when navigating to settings
   };
 
-  const toggleDropdown = () => {
-    setDropdownOpen(!dropdownOpen);
+  const togglesidebar = () => {
+    setsidebarOpen(!sidebarOpen);
   };
 
   return (
     <div className="App">
       <header className="App-header">
+      <button className="sidebar-toggle" onClick={togglesidebar}>
+          ☰
+        </button>
         <h1>
           <span className="app-title">B'FLOW</span>
         </h1>
         <h2>{userData?.name}</h2>
-
-        <div className="dropdown">
-          <button className="dropdown-toggle" onClick={toggleDropdown}>
-            ☰
-          </button>
-          {dropdownOpen && (
-            <div className="dropdown-menu">
-              <button className="settings-button" onClick={handleSettings}>
-                Settings
-              </button>
-              <button className="logout-button" onClick={handleLogout}>
-                Log Out
-              </button>
-            </div>
-          )}
-        </div>
       </header>
 
       <div className="app-content">
-        <nav className="nav">
+        <nav className={`nav ${sidebarOpen ? "open" : ""}`}>
           <button
             onClick={() => setActiveView("profile")}
             className={activeView === "profile" ? "active" : ""}
@@ -208,6 +193,12 @@ function App() {
               Link Child
             </button>
           )}
+          <button className="settings-button" onClick={handleSettings}>
+            Settings
+          </button>
+          <button className="logout-button" onClick={handleLogout}>
+            Log Out
+          </button>
         </nav>
         <main className="main">{renderView()}</main>
       </div>
